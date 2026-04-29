d372d42096e7bd8106c615a75a0de830ebec04fc
Andrew Kopf <magathia@gmail.com>
2026-04-29 16:44:38 -0700

    Should work now with the H2 database for testing purposes.

--- src/main/java/com/example/come_backend_story/ComeBackendStoryApplication.java
+++ src/main/java/com/example/come_backend_story/ComeBackendStoryApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ComeBackendStoryApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		// In your code
 		SpringApplication.run(ComeBackendStoryApplication.class, args);
 	}

94d295ca0b1befe9a5fcbbdd4d2ecd67ee4b3ea7
Andrew Kopf <magathia@gmail.com>
2026-04-30 10:15:32 -0700

    Save all the things, expaneded job code entity.  Why save space, we are keeping everything.

--- src/main/java/com/example/come_backend_story/entities/JobCodeEntity.java
+++ src/main/java/com/example/come_backend_story/entities/JobCodeEntity.java
@@ -20,7 +20,26 @@ public class JobCodeEntity {
   @Column(nullable = false, length = 80)
   private String name;
 
+  private Boolean active = true;
+
+  private String type;
+
+  private Boolean billable;
+
+  @Column(name = "assigned_to_all")
+  private Boolean assignedToAll;
+
+  @Column(name = "parent_id")
+  private Long parentId;
+
+  @Column(name = "short_code", length = 50)
+  private String shortCode;
+
+  @Column(name = "last_modified", nullable = false)
   private OffsetDateTime lastModified;
 
-  private Boolean active = true;
+  @Column(name = "synced_at")
+  private OffsetDateTime syncedAt;
+
+  private Boolean deleted = false;
 }
